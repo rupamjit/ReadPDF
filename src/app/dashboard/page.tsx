df0335e712eddab3243a7796a3985fcef28462dd
Rupamjit Ghosh <rupamjitghosh51@gmail.com>
2025-07-13 04:25:51 +0530

    updated auth-callback

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,5 @@
+import Dashboard from "@/components/Dashboard";
+import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
 
@@ -9,8 +11,18 @@ const DashBoardPage = async () => {
          redirect("/auth-callback?origin=dashboard")
     }
 
+    const dbUser = await db.user.findFirst({
+        where:{
+            id:user.id
+        }
+    })
+
+    if(!dbUser){
+        redirect("/auth-callback?origin=dashboard")
+    }
+
     return(
-        <div>{user.id}</div>
+        <Dashboard/>
     )
 }
 
