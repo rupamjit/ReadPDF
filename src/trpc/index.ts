37c7b25e9157f1803dd32ee8f6af8795547c10aa
Rupamjit Ghosh <rupamjitghosh51@gmail.com>
2025-07-13 14:46:52 +0530

    create user's file fetching trpc route

--- src/trpc/index.ts
+++ src/trpc/index.ts
@@ -1,5 +1,5 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
-import { publicProcedure, router } from "./trpc";
+import { privateProcedure, publicProcedure, router } from "./trpc";
 import { TRPCError } from "@trpc/server";
 import { db } from "@/db";
 
@@ -7,31 +7,40 @@ export const appRouter = router({
   authCallback: publicProcedure.query(async () => {
     const { getUser } = getKindeServerSession();
     const user = await getUser();
-    
+
     if (!user || !user.id || !user.email)
       throw new TRPCError({ code: "UNAUTHORIZED" });
 
     // check if the user in the database
 
     const dbUser = await db.user.findFirst({
-      where:{
-        id:user.id
-      }
-    })
+      where: {
+        id: user.id,
+      },
+    });
 
-    if(!dbUser){
+    if (!dbUser) {
       try {
         await db.user.create({
-          data:{
+          data: {
             id: user.id,
-            email: user.email
-          }
+            email: user.email,
+          },
         });
       } catch (error) {
         console.error("Error creating user:", error);
       }
     }
     return { success: true };
+  }),
+  getUserFiles: privateProcedure.query(async ({ ctx }) => {
+    const { userId } = ctx;
+
+    return await db.file.findMany({
+      where: {
+        userId,
+      },
+    });
   }),
 });
 
