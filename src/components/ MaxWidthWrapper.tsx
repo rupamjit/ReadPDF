6d4266e24a4ed08f9f1e45b853fcc30c8a4244a2
Rupamjit Ghosh <rupamjitghosh51@gmail.com>
2025-09-01 14:24:56 +0530

    completed the scadcnui setup

---
+++ src/components/ MaxWidthWrapper.tsx
@@ -0,0 +1,15 @@
+import React, { ReactNode } from 'react'
+
+const  MaxWidthWrapper = ({
+  className,
+  children,
+}: {
+  className?: string
+  children: ReactNode
+}) => {
+  return (
+    <div> MaxWidthWrapper</div>
+  )
+}
+
+export default  MaxWidthWrapper
