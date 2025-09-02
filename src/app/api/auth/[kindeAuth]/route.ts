02237646f152a40ac3e0be157c512f3ad80a6a04
Rupamjit Ghosh <rupamjitghosh51@gmail.com>
2025-09-02 19:58:37 +0530

    update kinde types

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -2,16 +2,11 @@ import { handleAuth } from '@kinde-oss/kinde-auth-nextjs/server'
 import { NextRequest } from 'next/server'
 
 
-interface AuthParams {
-  params: {
-    kindeAuth: string
-  }
-}
 
 
 export async function GET(
   request: NextRequest,
-  { params }: AuthParams
+  { params }:any
 ) {
   const endpoint = params.kindeAuth
   return handleAuth(request, endpoint)
